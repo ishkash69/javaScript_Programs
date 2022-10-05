6c17d31c5bcb2defe0b7bf0de218e8416bb0f8cb
ishkash69 <107914962+ishkash69@users.noreply.github.com>
2022-10-05 13:20:02 +0530

    adding a student

--- arrayOfObject.js
+++ arrayOfObject.js
@@ -1,26 +1,4 @@
-const addingStudents=(value)=>{
-    let studentDetails =[
-        {
-            name: 'rahul',
-            rollno:1,
-            address:'House No.1'
-        },
-        {
-            name: 'prem',
-            rollno:2,
-            address:'House No.2'
-        },
-    
-    ]
 
-    // console.log(studentDetails)
-    // adding a new object
-    
-    
-  
-    return studentDetails;
-}
-console.log(addingStudents())
 
 const deletingStudents=(value)=>{
     let studentDetails =[
@@ -49,11 +27,12 @@ const deletingStudents=(value)=>{
             return studentDetails
         }else{
             ifStudentExist =  false
-         
+            // console.log('no data found')
+            
         }
         // by using ternary operator
         // studentDetails[i].rollno===value? studentDetails.splice(i,1):console.log("no data exist")
     }
     return studentDetails
 }
-console.log(deletingStudents(2))
+console.log(deletingStudents(4))
